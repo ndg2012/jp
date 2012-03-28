608f64f4cd8cc7e4525ae5f893585ed848cf1eb0
Mateusz Czerwiński <mczerwinski@p210-18.labpk.inf.ug.edu.pl>
2012-03-28 15:03:35 +0200

    update

--- laboratoria/lab6/imiona.c
+++ laboratoria/lab6/imiona.c
@@ -1,33 +1,18 @@
-/*
- * Prymitywna wersja 
- * strncmp 
- */
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef unsigned long long ull;
-
-const int ile = 4960;
-
 
 int
 main (int argc, char *argv[])
 {
-  ull i, t;
+  unsigned int i;
 
-  char wzorzec[] = "ki";
-  char tablica[ile][20];
+  char tablica[20];
 
-  for (i = 0; i < ile; i++)
-    scanf ("%s", &tablica[i]);
-
-
-  for (i = 0; i < ile; i++)
-    if (!strncmp (tablica[i], wzorzec, strlen (wzorzec)))
-      printf ("%s\n", tablica[i]);
-
+  while (scanf ("%s", tablica) == 1)
+    {
+      if (!strncmp (tablica, argv[1], strlen (argv[1])))
+	printf ("%s\n", tablica);
+    }
 
 
   return 0;
