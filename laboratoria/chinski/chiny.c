5103211a083b6bdbb429cbcd8c930c5cc041b8d9
Mateusz Czerwiński <mtczerwinski@gmail.com>
2012-04-12 16:39:09 +0200

    poprawki

--- laboratoria/chinski/chiny.c
+++ laboratoria/chinski/chiny.c
@@ -1,99 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 int size = 9;
-char tablica[100];
 
 int word_int(char *tablica[], int i, char wskaznik);
-void usun_spacje(char *tekst, char *po);
+void usun_spacje(char *tekst);
 void sstr(char *string, int rozmiar);
 
 void token(char *chinskie, char *slownik[])
 {
     extern int size;
     int i;
-    int numer2;
+    int numer;
     int rozmiar;
-    
-    while (strlen(chinskie) > 1) {
+    char tablica[100] = {'\0'};
+
+    while ((int)strlen(chinskie) > 1) {
 	char wskaznik = chinskie[0];
 	for (i = 0; i < size; i++) {
-	    numer2 = word_int(slownik, i, wskaznik);
-	    rozmiar = strlen(slownik[numer2]);
-	    if (!strncmp(chinskie, slownik[numer2], rozmiar)) {
-	      //  strcpy(tablica,slownik[numer2]);
-	      //  strcpy(tablica," ");
-	      printf("%s ", slownik[numer2]);
-	      sstr(chinskie, rozmiar);
+	    numer = word_int(slownik, i, wskaznik);
+	    rozmiar =(int)strlen(slownik[numer]);
+	    if (!strncmp(chinskie, slownik[numer], rozmiar)) {
+		strcat(tablica, slownik[numer]);
+		strcat(tablica, " ");
+		sstr(chinskie, rozmiar);
 	    }
 	}
     }
-    puts("");
+    tablica[strlen(tablica) - 1] = '\0';
+    strcpy(chinskie, tablica);
 }
-
 
 int main(int argc, char *argv[])
 {
+    char *slownik[] = { "ani", "czekolady", "dasz", "jesli", "kakao", "krowie", "nie","mleka","wydoisz"};
+    char string[] = "jesli krowie dasz kakao nie wydoisz czekolady ani mleka";
 
-    char *slownik[] =
-	{ "czekolady", "dasz", "jesli", "kakao", "krowie", "nie",
-	  "wydoisz", "ani", "mleka"
-    };
-    char *przed = "jesli krowie dasz kakao nie wydoisz czekolady ani mleka";
-    char po[100];
+    printf("%d %s\n", (int)strlen(string), string);
+    usun_spacje(string);    
+    printf("%d %s\n", (int)strlen(string), string);
+    token(string, slownik);
+    printf("%d %s\n", (int)strlen(string), string);
 
-
-    usun_spacje(przed, po);
-
-    printf("%s\n%s\n",przed,po);
-    token(po, slownik);
-    //	puts(tablica);
-    printf("%s",tablica);
     return 0;
 }
-
 
 void sstr(char *string, int rozmiar)
 {
     int i, j;
     char temp[100];
-    //char *temp = malloc(strlen(string) * sizeof(char));
 
-    for (i = rozmiar, j = 0; string[i] != '\0'; i++, j++) {
+    for (i = rozmiar, j = 0; string[i] != '\0'; i++, j++)
 	temp[j] = string[i];
-
-    }
     temp[j] = '\0';
-
-    strcpy(string,temp);
-
-    
-    // free(temp);
-    
+    strcpy(string, temp);
 }
 
-
-
-void usun_spacje(char *tekst, char *po)
+void usun_spacje(char *tekst)
 {
     int length = strlen(tekst);
-    int j;
-    int i = 0;
+    int j= 0, i = 0;
+    char temp[100];
 
     j = i;
     for (i = 0; i < length; i++)
 	if (tekst[i] != ' ') {
-	    po[j] = tekst[i];
+	    temp[j] = tekst[i];
 	    j++;
 	}
+    temp[j] = '\0';
+    strcpy(tekst,temp);
 }
 
 int word_int(char *tablica[], int i, char wskaznik)
 {
-    if (tablica[i][0] == wskaznik)
-	return i;
-    else
-	return -1;
+    if (tablica[i][0] == wskaznik) return i;
+    // else return -1;
 }
