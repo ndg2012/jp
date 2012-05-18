28edf17a19255f655d1ce2e42fcd406cc88055b0
Mateusz Czerwiński <mtczerwinski@gmail.com>
2012-05-18 12:06:14 +0200

    poprawki wiecej testow

--- laboratoria/struktury/SIL.c
+++ laboratoria/struktury/SIL.c
@@ -45,33 +45,62 @@ int main(void)
 	scanf("%d %d %d %d", &b.ld.x, &b.ld.y, &b.pg.x, &b.pg.y);
 
 
-
-
 	if(dis(a,b) == 1)
-	  printf("1. %d\n", area(a) + area(b));
+	  printf("%d\n", area(a) + area(b));
 	else {
 	  if(in(a,b) == 1)
-	    printf("2. %d\n", area(a) > area(b) ? area(a) : area(b));
+	    printf("%d\n", area(a) > area(b) ? area(a) : area(b));
 	  else {
 	    if(a.pg.x <= b.pg.x && a.pg.y <= b.pg.y){
 	      da = a.pg.x - b.ld.x;
 	      db = a.pg.y - b.ld.y;
-	      printf("3. %d\n",area(a)+area(b)-da*db);
+	      printf("%d\n",area(a)+area(b)-da*db);
 	    }
-	    else if(a.ld.y == b.ld.y){ // szwankuje
-	      da = b.pg.x - b.ld.x;
-	      db = a.pg.y - a.ld.y;
-	      printf("4. %d\n",area(a)+area(b)-da*db);
+	    else if(a.ld.y == b.ld.y){
+	      if(b.pg.x <= a.pg.x && b.pg.y <=  a.pg.y){
+		da = b.pg.x - a.ld.x;
+		db = b.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      } else {
+		da = a.pg.x - a.ld.x;
+		db = b.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      }
  	    }
-	    else if(a.pg.x == b.pg.x){ // szwankuje
-	      da = a.pg.x - a.ld.x;
-	      db = b.pg.y - a.ld.y;
-	      printf("5. %d\n",area(a)+area(b)-da*db);
+	    else if(a.pg.y == b.pg.y){
+	    	da = b.pg.x - a.ld.x;
+		db = a.pg.y - a.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
 	    }
-	    else if( a.pg.x >= b.pg.x && a.pg.y <= b.pg.y){
-	      da = b.pg.x - a.ld.x;
-	      db = a.pg.y - b.ld.y;
-	      printf("6. %d\n",area(a)+area(b)-da*db);
+	    else if(a.ld.x == b.ld.x){
+	      if(b.pg.x <= a.pg.x && b.pg.y <= a.pg.y){
+		da = b.pg.x - b.ld.x;
+		db = b.pg.y - a.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      } else {
+		da = a.pg.x - a.ld.x;
+		db = b.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      }
+	    }
+	    else if( a.pg.x >= b.pg.x && a.pg.y <= b.pg.y && b.ld.y >= a.ld.y){
+	      if(a.ld.x <= b.ld.x && b.pg.x <= a.pg.x){
+		da = b.pg.x - b.ld.x;
+		db = a.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      } else {
+		da = b.pg.x - a.ld.x;
+		db = a.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      }
+	    }else if(a.ld.x <= b.ld.x && a.pg.x <= b.pg.x){
+	      if(a.ld.y <= b.ld.y && b.pg.y <= a.pg.y){
+		da = a.pg.x - b.ld.x;
+		db = b.pg.y - b.ld.y;
+		printf("%d\n",area(a)+area(b)-da*db);
+	      }
+	      else
+		fprintf(stderr,"Error2. Nieznany przypadek.\n");
 	    } else
 	    fprintf(stderr,"Error. Nieznany przypadek.\n");
 	  }
